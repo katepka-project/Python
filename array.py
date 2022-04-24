28fa214b7023d9df00d783ce8d3202856338cebc
Kate kyungae Park <72452744+katepka-project@users.noreply.github.com>
2022-04-23 20:25:26 -0400

    Create array.py

---
+++ array.py
@@ -0,0 +1,16 @@
+ n=3
+ m=4
+ array = [i for i in range(10)]
+ print(array)
+ 
+ #2demension
+ n=3
+ m=4
+ array = [[0] * m for _ in range(n)]
+ print(array)
+ 
+ #roof using _
+ for _ in range(5):
+  print("Hi")
+  
+  
